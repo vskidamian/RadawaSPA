47ac349444ebc46c7fbac1f82aa66d11f67b3aab
vskidamian <jakubowski1337@gmail.com>
2020-06-28 00:37:14 +0200

    Header, Navbar and mobile hamburger. Mobile section: About

---
+++ app.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+const body = document.body;
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("is-active")
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+    body.classList.toggle("overy");
+});
