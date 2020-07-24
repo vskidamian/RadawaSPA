93c09dde385ba550bc4238f428d0597f31603e60
vskidamian <jakubowski1337@gmail.com>
2020-07-24 21:41:34 +0200

    Form Email

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 const body = document.body;
+const galleryTags = document.querySelectorAll(".gallery a");
+const seeMore = document.querySelector("#galeria-btn")
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("is-active")
@@ -22,3 +24,7 @@ links.forEach(link => {
         body.classList.toggle("overy");
     })
 })
+
+seeMore.addEventListener("click", () => {
+
+});
--- gallery.js
+++ gallery.js
@@ -5,5 +5,5 @@ $(document).ready(function () {
         gallery: {
             enabled: true
         }
-    });
+    })
 })
